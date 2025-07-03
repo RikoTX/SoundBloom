bd5a0a90a3d37001f89d0120aae6db59b37737c4
project Amir <amirzhanchikk@gmail.com>
2025-07-03 17:09:38 +0500

    Добавил таблицу

--- src/components/Content/Home/Home.jsx
+++ src/components/Content/Home/Home.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
     SearchOutlined,
 } from '@ant-design/icons';
-import musicData from '../../../../music.json';
+import musicData from '../../../../public/music.json';
+
+import {
+    HeartOutlined
+} from '@ant-design/icons';
+
+
+
 
 const imageWrapperStyle = {
     position: 'relative',
@@ -23,7 +30,7 @@ const inputContainerStyle = {
     display: 'flex',
     borderRadius: '8px',
     overflow: 'hidden',
-    width: '400px', 
+    width: '400px',
     backgroundColor: '#121212',
     border: '1px solid #333',
 };
@@ -160,13 +167,17 @@ export default function Home() {
     const handleSearch = () => {
         console.log(searchValue);
     };
+    const numbers = [];
+    for (let i = 1; i <= 7; i++) {
+        numbers.push(<div key={i}>#{i}</div>);
+    }
 
     return (
         <div>
             <div style={ImageCenter}>
                 <div style={imageWrapperStyle}>
                     <img
-                        src="src/assets/fon.png"
+                        src="../../../public/fon.png"
                         alt="SoundBloom"
                         style={imageStyle}
                     />
@@ -235,13 +246,13 @@ export default function Home() {
                                     style={{ width: '150px', height: '150px', borderRadius: '10px' }}
                                 />
                                 <p style={{ margin: '5px 0', fontWeight: 600 }}>{song.title}</p>
-                                <p style={{ margin: 0, color: 'white', color:'#929292', fontSize: '12px' }}>{song.artist}</p>
+                                <p style={{ margin: 0, color: '#929292', fontSize: '12px' }}>{song.artist}</p>
                             </div>
                         ))}
                         <div style={{
                             display: 'flex',
                             alignItems: 'center',
-                            gap: '20px' 
+                            gap: '20px'
                         }}>
                             <div style={{
                                 display: 'flex',
@@ -290,13 +301,13 @@ export default function Home() {
                                     style={{ width: '150px', height: '150px', borderRadius: '10px' }}
                                 />
                                 <p style={{ margin: '5px 0', fontWeight: 600 }}>{song.title}</p>
-                                <p style={{ margin: 0, color: 'white', color:'#929292', fontSize: '12px' }}>{song.artist}</p>
+                                <p style={{ margin: 0, color: '#929292', fontSize: '12px' }}>{song.artist}</p>
                             </div>
                         ))}
                         <div style={{
                             display: 'flex',
                             alignItems: 'center',
-                            gap: '20px' 
+                            gap: '20px'
                         }}>
                             <div style={{
                                 display: 'flex',
@@ -328,6 +339,119 @@ export default function Home() {
                                 </p>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div >
+                    <p style={{ fontWeight: 600, fontSize: 35, margin: '35px 10px 0px 4%' }}>
+                        Trending <span style={{ color: '#cb0094' }}>Songs</span>
+                    </p>
+                    <div style={{ marginLeft: '370px' }}>
+                        <div style={{ display: 'flex', justifyContent: 'space-evenly', gap: '26%', width: '90%' }}>
+                            <p style={{ fontWeight: 500, fontSize: 17 }} >Relase Date</p>
+                            <p style={{ fontWeight: 500, fontSize: 17 }} >Album</p>
+                            <p style={{ fontWeight: 500, fontSize: 17 }} >Time</p>
+                        </div>
+                    </div>
+
+                    <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', fontSize: '30px', fontWeight: 500, gap: '48px', marginLeft: '20px', marginBottom: '10px' }}>
+                            {numbers}
+                        </div>
+                        <div style={{ display: 'flex', flexDirection: 'column', paddingRight: '2%', width: '90%' }}>
+                            {musicData.TrendingSongs.map((song, index) => (
+                                <div key={index} style={{ width: '100%', backgroundColor: '#1F1F1F', borderRadius: '10px', margin: '10px' }}>
+                                    <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                                        <div style={{ display: 'flex', justifyContent: 'space-around', width: '23%' }}>
+                                            <img
+                                                src={song.cover}
+                                                alt={song.title}
+                                                style={{ width: '70px', height: '70px', borderRadius: '10px' }}
+                                            />
+                                            <div style={{ width: '100%', marginLeft: '5%' }}>
+                                                <p style={{ margin: '5px 0', fontWeight: 600, fontSize: '120%' }}>{song.title}</p>
+                                                <p style={{ margin: 0, color: '#929292', fontSize: '12px' }}>{song.artist}</p>
+                                            </div>
+
+                                        </div>
+                                        <div style={{
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            width: '100%',
+                                            justifyContent: 'space-around',
+                                            paddingLeft: '50px',
+                                            gap: '40px',
+                                        }}>
+                                            <p style={{
+                                                width: '120px',
+                                                margin: '5px 0',
+                                                fontWeight: 400,
+                                                fontSize: '14px',
+                                                whiteSpace: 'nowrap',
+                                                overflow: 'hidden',
+                                                textOverflow: 'ellipsis'
+                                            }}>
+                                                {song.releaseDate}
+                                            </p>
+
+                                            <p style={{
+                                                width: '200px',
+                                                margin: '5px 0',
+                                                fontWeight: 400,
+                                                whiteSpace: 'nowrap',
+                                                overflow: 'hidden',
+                                                textOverflow: 'ellipsis'
+                                            }}>
+                                                {song.album}
+                                            </p>
+
+                                            <p style={{
+                                                width: '100px',
+                                                margin: '5px 0',
+                                                fontWeight: 400,
+                                                display: 'flex',
+                                                alignItems: 'center',
+                                                whiteSpace: 'nowrap',
+                                                overflow: 'hidden',
+                                                textOverflow: 'ellipsis',
+                                            }}>
+                                                <HeartOutlined style={{ marginRight: '10px', cursor: 'pointer', fontSize: '18px', color: '#EE10B0' }} />
+                                                {song.time}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <button style={{
+                            backgroundColor: '#1E1E1E',
+                            width: '120px',
+                            borderRadius: '5px',
+                            height: '40px',
+                            fontSize: '30px',
+                            border: 'none',
+                            color: 'white',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            cursor: 'pointer'
+                        }}>
+                            +<p style={{
+                                marginLeft: '8px',
+                                fontWeight: 500,
+                                fontSize: '15px',
+                                color: 'white'
+                            }}>
+                                View All
+                            </p>
+                        </button>
                     </div>
                 </div>
             </div>
