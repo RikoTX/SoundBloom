7fb6aefb05da18b2b02ad0196ea72b7b6fccaf44
project Amir <amirzhanchikk@gmail.com>
2025-07-08 18:40:07 +0500

    изменил данные

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Layout } from 'antd';
 import Sider from './components/Sider/Sider';
-import ContentPage from './components/Content/ContentPage' 
+import ContentPage from './components/Content/ContentPage'
 
 
 const outerLayoutStyle = {
@@ -13,11 +13,14 @@ const outerLayoutStyle = {
 
 export default function App() {
   const [activeButton, setActivePage] = useState('Home');
-
+  const [selectedAlbum, setSelectedAlbum] = useState(null); 
   return (
     <div style={outerLayoutStyle}>
       <Sider activeButton={activeButton} setActiveButton={setActivePage} />
-      <ContentPage activeButton={activeButton} />
+      <ContentPage activeButton={activeButton}
+        setActiveButton={setActivePage}
+        selectedAlbum={selectedAlbum}
+        setSelectedAlbum={setSelectedAlbum} />
     </div>
   );
 }
