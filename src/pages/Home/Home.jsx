0a3e56069a8fcb43c737fcb0d799e8fdf876cdc6
project Amir <amirzhanchikk@gmail.com>
2025-07-15 16:35:44 +0500

    стили изменил

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,182 +4,6 @@ import musicData from "../../data/music.json";
 import { HeartOutlined } from "@ant-design/icons";
 import usePlayerControls from "../../hooks/usePlayerControls";
 
-const submitGoogleButtonStyle = {
-  padding: "15px",
-  fontSize: "16px",
-  backgroundColor: "transparent",
-  color: "white",
-  border: "3px solid #fff",
-  borderRadius: "8px",
-  cursor: "pointer",
-  marginTop: "20px",
-};
-const fieldStyle = {
-  padding: "10px",
-  fontSize: "15px",
-  border: "3px solid #D9D9D9",
-  borderRadius: "4px",
-  backgroundColor: "transparent",
-  color: "white",
-};
-
-const submitButtonStyle = {
-  padding: "15px",
-  fontSize: "16px",
-  backgroundColor: "#cb0094",
-  color: "white",
-  border: "none",
-  borderRadius: "8px",
-  cursor: "pointer",
-  marginTop: "20px",
-};
-
-const imageWrapperStyle = {
-  position: "relative",
-  maxWidth: "100%",
-};
-
-const imageStyle = {
-  paddingRight: "50px",
-  maxWidth: "100%",
-  width: "1170px",
-  height: "auto",
-  display: "block",
-};
-const inputContainerStyle = {
-  position: "absolute",
-  top: "8%",
-  left: "20%",
-  transform: "translate(-50%, -50%)",
-  display: "flex",
-  borderRadius: "8px",
-  overflow: "hidden",
-  width: "400px",
-  backgroundColor: "#121212",
-  border: "1px solid #333",
-};
-
-const inputStyle = {
-  flex: 1,
-  padding: "10px 16px",
-  border: "none",
-  fontSize: "14px",
-  outline: "none",
-  backgroundColor: "#121212",
-  color: "grey",
-  width: "100%",
-};
-
-const buttonStyle = {
-  padding: "8px 16px",
-  border: "1px solid #121212",
-  backgroundColor: "#121212",
-  color: "#fff",
-  cursor: "pointer",
-  fontWeight: "bold",
-};
-
-const ImageCenter = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  padding: "20px",
-  paddingTop: "40px",
-};
-const linkText = {
-  display: "flex",
-  justifyContent: "center",
-  gap: "30px",
-  marginTop: "20px",
-};
-
-const linkStyle = {
-  fontSize: "17px",
-  color: "white",
-  textDecoration: "none",
-};
-
-const Position = {
-  position: "absolute",
-  top: "2.5%",
-  right: "38%",
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-};
-
-const ButtonElements = {
-  position: "absolute",
-  top: "4%",
-  right: "10%",
-};
-
-const styleButtonLogin = {
-  backgroundColor: "transparent",
-  color: "#cb0094",
-  padding: "11px",
-  width: "150px",
-  border: "1px solid #cb0094",
-  borderRadius: "3px",
-  marginRight: "10px",
-  cursor: "pointer",
-};
-const styleButtonSignUp = {
-  backgroundColor: "#cb0094",
-  padding: "11px",
-  width: "150px",
-  borderRadius: "3px",
-  border: "1px solid #cb0094",
-  cursor: "pointer",
-};
-
-const textStyle = {
-  fontWeight: 700,
-  fontSize: 40,
-};
-const textPosition = {
-  position: "absolute",
-  top: "20%",
-  left: "3%",
-};
-
-const textImage = {
-  width: "380px",
-  position: "absolute",
-  bottom: "30%",
-  left: "3%",
-  color: "#bdbdbd",
-};
-
-const positionButton = {
-  position: "absolute",
-  bottom: "20%",
-  left: "3.5%",
-};
-
-const styleDiscoverNow = {
-  color: "white",
-  backgroundColor: "#cb0094",
-  border: "1px solid #cb0094",
-  padding: "10px",
-  width: "150px",
-  borderRadius: "4px",
-  fontSize: "17px",
-  marginRight: "30px",
-  cursor: "pointer",
-};
-
-const styleCreatePlaylist = {
-  color: "#0E9EEF",
-  backgroundColor: "transparent",
-  border: "2px solid #0E9EEF",
-  padding: "10px",
-  width: "150px",
-  borderRadius: "4px",
-  fontSize: "17px",
-  cursor: "pointer",
-};
 export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
   const [activeTab, setActiveTab] = useState("signup");
   const [searchValue, setSearchValue] = useState("");
@@ -217,19 +41,70 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
 
   return (
     <div>
-      <div style={ImageCenter}>
-        <div style={imageWrapperStyle}>
-          <img src="fon.png" alt="SoundBloom" style={imageStyle} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "20px",
+          paddingTop: "40px",
+        }}
+      >
+        <div style={{ position: "relative", maxWidth: "100%" }}>
+          <img
+            src="fon.png"
+            alt="SoundBloom"
+            style={{
+              paddingRight: "50px",
+              maxWidth: "100%",
+              width: "1170px",
+              height: "auto",
+              display: "block",
+            }}
+          />
 
-          <div style={inputContainerStyle}>
+          <div
+            style={{
+              position: "absolute",
+              top: "8%",
+              left: "20%",
+              transform: "translate(-50%, -50%)",
+              display: "flex",
+              borderRadius: "8px",
+              overflow: "hidden",
+              width: "400px",
+              backgroundColor: "#121212",
+              border: "1px solid #333",
+            }}
+          >
             <input
               type="text"
               value={searchValue}
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search For Musics, Artist, ..."
-              style={inputStyle}
+              style={{
+                flex: 1,
+                padding: "10px 16px",
+                border: "none",
+                fontSize: "14px",
+                outline: "none",
+                backgroundColor: "#121212",
+                color: "grey",
+                width: "100%",
+              }}
             />
-            <button onClick={handleSearch} style={buttonStyle}>
+            <button
+              onClick={handleSearch}
+              style={{
+                padding: "8px 16px",
+                border: "1px solid #121212",
+                backgroundColor: "#121212",
+                color: "#fff",
+                cursor: "pointer",
+                fontWeight: "bold",
+              }}
+            >
               <SearchOutlined />
             </button>
           </div>
@@ -287,32 +162,101 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
               )}
             </div>
           )}
-          <div style={Position}>
-            <div style={linkText}>
-              <a href="#" style={linkStyle}>
+          <div
+            style={{
+              position: "absolute",
+              top: "2.5%",
+              right: "38%",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                gap: "30px",
+                marginTop: "20px",
+              }}
+            >
+              <a
+                href="#"
+                style={{
+                  fontSize: "17px",
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
                 About Us
               </a>
-              <a href="#" style={linkStyle}>
+              <a
+                href="#"
+                style={{
+                  fontSize: "17px",
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
                 Contact
               </a>
-              <a href="#" style={linkStyle}>
+              <a
+                href="#"
+                style={{
+                  fontSize: "17px",
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
                 Premium
               </a>
             </div>
           </div>
-          <div style={ButtonElements}>
-            <button style={styleButtonLogin}>Login</button>
-            <button style={styleButtonSignUp}>Sign Up</button>
+          <div style={{ position: "absolute", top: "4%", right: "10%" }}>
+            <button
+              style={{
+                backgroundColor: "transparent",
+                color: "#cb0094",
+                padding: "11px",
+                width: "150px",
+                border: "1px solid #cb0094",
+                borderRadius: "3px",
+                marginRight: "10px",
+                cursor: "pointer",
+              }}
+            >
+              Login
+            </button>
+            <button
+              style={{
+                backgroundColor: "#cb0094",
+                padding: "11px",
+                width: "150px",
+                borderRadius: "3px",
+                border: "1px solid #cb0094",
+                cursor: "pointer",
+              }}
+            >
+              Sign Up
+            </button>
           </div>
 
-          <div style={textPosition}>
-            <p style={textStyle}>
+          <div style={{ position: "absolute", top: "20%", left: "3%" }}>
+            <p style={{ fontWeight: 700, fontSize: 40 }}>
               All the <span style={{ color: "#cb0094" }}>Best Songs</span>
               <br />
               in One Place
             </p>
           </div>
-          <div style={textImage}>
+          <div
+            style={{
+              width: "380px",
+              position: "absolute",
+              bottom: "30%",
+              left: "3%",
+              color: "#bdbdbd",
+            }}
+          >
             <p>
               On our website, you can access an amazing collection of popular
               and new songs. Stream your favorite tracks in high quality and
@@ -320,9 +264,36 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
               it all for you!
             </p>
           </div>
-          <div style={positionButton}>
-            <button style={styleDiscoverNow}>Discover Now</button>
-            <button style={styleCreatePlaylist}>Create Playlist</button>
+          <div style={{ position: "absolute", bottom: "20%", left: "3.5%" }}>
+            <button
+              style={{
+                color: "white",
+                backgroundColor: "#cb0094",
+                border: "1px solid #cb0094",
+                padding: "10px",
+                width: "150px",
+                borderRadius: "4px",
+                fontSize: "17px",
+                marginRight: "30px",
+                cursor: "pointer",
+              }}
+            >
+              Discover Now
+            </button>
+            <button
+              style={{
+                color: "#0E9EEF",
+                backgroundColor: "transparent",
+                border: "2px solid #0E9EEF",
+                padding: "10px",
+                width: "150px",
+                borderRadius: "4px",
+                fontSize: "17px",
+                cursor: "pointer",
+              }}
+            >
+              Create Playlist
+            </button>
           </div>
         </div>
       </div>
@@ -1200,7 +1171,14 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
                       <input
                         type="tel"
                         placeholder="Format: +7 (123) 456 78 99"
-                        style={fieldStyle}
+                        style={{
+                          padding: "10px",
+                          fontSize: "15px",
+                          border: "3px solid #D9D9D9",
+                          borderRadius: "4px",
+                          backgroundColor: "transparent",
+                          color: "white",
+                        }}
                       />
                     </div>
                     <div
@@ -1219,10 +1197,30 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
                       <input
                         type="password"
                         placeholder="Enter Your Password"
-                        style={fieldStyle}
+                        style={{
+                          padding: "10px",
+                          fontSize: "15px",
+                          border: "3px solid #D9D9D9",
+                          borderRadius: "4px",
+                          backgroundColor: "transparent",
+                          color: "white",
+                        }}
                       />
                     </div>
-                    <button style={submitButtonStyle}>Login</button>
+                    <button
+                      style={{
+                        padding: "15px",
+                        fontSize: "16px",
+                        backgroundColor: "#cb0094",
+                        color: "white",
+                        border: "none",
+                        borderRadius: "8px",
+                        cursor: "pointer",
+                        marginTop: "20px",
+                      }}
+                    >
+                      Login
+                    </button>
                     <div
                       style={{
                         display: "flex",
@@ -1257,7 +1255,13 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
                     </div>
                     <button
                       style={{
-                        ...submitGoogleButtonStyle,
+                        fontSize: "16px",
+                        backgroundColor: "transparent",
+                        color: "white",
+                        border: "3px solid #fff",
+                        borderRadius: "8px",
+                        cursor: "pointer",
+                        marginTop: "20px",
                         display: "flex",
                         alignItems: "center",
                         justifyContent: "center",
@@ -1316,7 +1320,14 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
                           <input
                             type="text"
                             placeholder="Enter Your Name"
-                            style={fieldStyle}
+                            style={{
+                              padding: "10px",
+                              fontSize: "15px",
+                              border: "3px solid #D9D9D9",
+                              borderRadius: "4px",
+                              backgroundColor: "transparent",
+                              color: "white",
+                            }}
                           />
                         </div>
                         <div
@@ -1338,7 +1349,14 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
                           <input
                             type="email"
                             placeholder="Enter Your E-Mail"
-                            style={fieldStyle}
+                            style={{
+                              padding: "10px",
+                              fontSize: "15px",
+                              border: "3px solid #D9D9D9",
+                              borderRadius: "4px",
+                              backgroundColor: "transparent",
+                              color: "white",
+                            }}
                           />
                         </div>
                       </div>
@@ -1359,7 +1377,14 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
                       <input
                         type="tel"
                         placeholder="Format: +7 (123) 456 78 99"
-                        style={fieldStyle}
+                        style={{
+                          padding: "10px",
+                          fontSize: "15px",
+                          border: "3px solid #D9D9D9",
+                          borderRadius: "4px",
+                          backgroundColor: "transparent",
+                          color: "white",
+                        }}
                       />
                     </div>
                     <div
@@ -1378,10 +1403,30 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
                       <input
                         type="password"
                         placeholder="Enter Your Password"
-                        style={fieldStyle}
+                        style={{
+                          padding: "10px",
+                          fontSize: "15px",
+                          border: "3px solid #D9D9D9",
+                          borderRadius: "4px",
+                          backgroundColor: "transparent",
+                          color: "white",
+                        }}
                       />
                     </div>
-                    <button style={submitButtonStyle}>Sign Up</button>
+                    <button
+                      style={{
+                        padding: "15px",
+                        fontSize: "16px",
+                        backgroundColor: "#cb0094",
+                        color: "white",
+                        border: "none",
+                        borderRadius: "8px",
+                        cursor: "pointer",
+                        marginTop: "20px",
+                      }}
+                    >
+                      Sign Up
+                    </button>
                     <div
                       style={{
                         display: "flex",
@@ -1416,7 +1461,13 @@ export default function Home({ setCurrentTrackIndex, setCurrentPlaylist }) {
                     </div>
                     <button
                       style={{
-                        ...submitGoogleButtonStyle,
+                        fontSize: "16px",
+                        backgroundColor: "transparent",
+                        color: "white",
+                        border: "3px solid #fff",
+                        borderRadius: "8px",
+                        cursor: "pointer",
+                        marginTop: "20px",
                         display: "flex",
                         alignItems: "center",
                         justifyContent: "center",
