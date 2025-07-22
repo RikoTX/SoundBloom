3a3d6aac8c3d1e659aef32daa8e46c7f4561682c
project Amir <amirzhanchikk@gmail.com>
2025-07-22 11:33:32 +0500

    add grid PlaylistGrid

--- src/components/PlaylistGrid/PlaylistGrid.jsx
+++ src/components/PlaylistGrid/PlaylistGrid.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Row, Col } from "antd";
 import ViewAllCircleButton from "../../components/ViewAllCircleButton/ViewAllCircleButton";
 
-const PlaylistGrid = ({ title, pinkTitle, playlist = [], showAll, setShowAll }) => {
+const PlaylistGrid = ({
+  title,
+  pinkTitle,
+  playlist = [],
+  showAll,
+  setShowAll,
+}) => {
   const playlistToShow = showAll ? playlist : playlist.slice(0, 5);
 
   return (
@@ -10,53 +17,47 @@ const PlaylistGrid = ({ title, pinkTitle, playlist = [], showAll, setShowAll }) 
         {title} <span style={{ color: "#cb0094" }}>{pinkTitle}</span>
       </p>
 
-      <div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            flexWrap: "wrap",
-            paddingRight: "40px",
-            paddingLeft: "40px",
-          }}
-        >
+      <div style={{ padding: "0 40px" }}>
+        <Row justify="space-around" align="align" gutter={[20, 20]}>
           {playlistToShow.map((playlist, index) => (
-            <div
-                    key={index}
-                    style={{
-                      textAlign: "center",
-                      backgroundColor: "#1F1F1F",
-                      borderRadius: "10px",
-                    }}
-                  >
-                    <img
-                      src={playlist.cover}
-                      alt={playlist.titlePlaylist}
-                      style={{
-                        width: "170px",
-                        height: "160px",
-                        borderRadius: "10px",
-                      }}
-                    />
-                    <p
-                      style={{
-                        margin: "5px 0",
-                        fontWeight: 600,
-                        display: "flex",
-                        justifyContent: "flex-start",
-                        padding: " 3px",
-                        paddingLeft: "8px",
-                      }}
-                    >
-                      {playlist.titlePlaylist}
-                    </p>
-                  </div>
+            <Col key={index} >
+              <div
+                style={{
+                  textAlign: "center",
+                  backgroundColor: "#1F1F1F",
+                  borderRadius: "10px",
+                }}
+              >
+                <img
+                  src={playlist.cover}
+                  alt={playlist.titlePlaylist}
+                  style={{
+                    width: "100%",
+                    height: "160px",
+                    borderRadius: "10px",
+                    objectFit: "cover",
+                  }}
+                />
+                <p
+                  style={{
+                    margin: "5px 0",
+                    fontWeight: 600,
+                    textAlign: "left",
+                    padding: "3px 8px",
+                  }}
+                >
+                  {playlist.titlePlaylist}
+                </p>
+              </div>
+            </Col>
           ))}
 
           {!showAll && playlist.length > 4 && (
-            <ViewAllCircleButton onToggle={() => setShowAll(true)} />
+            <Col style={{ display: "flex", justifyContent: "center", paddingLeft:'40px' }}>
+              <ViewAllCircleButton onToggle={() => setShowAll(true)} />
+            </Col>
           )}
-        </div>
+        </Row>
       </div>
     </div>
   );
