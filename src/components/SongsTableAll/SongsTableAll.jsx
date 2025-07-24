c5c2d7b07a082fd50d80899c3dac8ffa8232e78e
project Amir <amirzhanchikk@gmail.com>
2025-07-24 15:57:43 +0500

    изменил таблицу

--- src/components/SongsTableAll/SongsTableAll.jsx
+++ src/components/SongsTableAll/SongsTableAll.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from "antd";
 import { HeartOutlined } from "@ant-design/icons";
 
 const SongsTableAll = ({ title, pinkTitle, songs = [], columns = [] }) => {
@@ -8,134 +9,77 @@ const SongsTableAll = ({ title, pinkTitle, songs = [], columns = [] }) => {
       : songs;
 
   return (
-    <div>
-      <p style={{ fontWeight: 600, fontSize: 35, margin: "35px 10px 0px 4%" }}>
+    <div style={{ padding: "0 5%" }}>
+      <p style={{ fontWeight: 600, fontSize: 35, marginTop: 35 }}>
         {title} <span style={{ color: "#cb0094" }}>{pinkTitle}</span>
       </p>
 
-      <div style={{ marginLeft: "490px" }}>
-        <div
+      <Row
+        gutter={16}
+        style={{ margin: "30px 0", fontWeight: 500, fontSize: 20 }}
+      >
+        <Col span={2}></Col>
+        <Col span={8}></Col>
+        {columns.map((col, idx) => (
+          <Col
+            key={idx}
+            span={col.span || 4}
+            style={{ textAlign: "center", fontSize: 20 }}
+          >
+            {col.header}
+          </Col>
+        ))}
+      </Row>
+
+      {sortedSongs.map((song, index) => (
+        <Row
+          key={index}
+          gutter={16}
+          align="middle"
           style={{
-            display: "flex",
-            alignItems: "center",
-            width: "90%",
-            gap: "130px",
+            background: "#1F1F1F",
+            borderRadius: 10,
+            marginBottom: 12,
           }}
         >
-          {columns.map((col, idx) => (
-            <div
-              key={idx}
-              style={{
-                width: col.width || "150px",
-                textAlign: "center",
-                fontWeight: 500,
-                fontSize: 20,
-              }}
-            >
-              {col.header}
-            </div>
-          ))}
-        </div>
-      </div>
+          <Col
+            span={2}
+            style={{ fontSize: 24, fontWeight: 600, textAlign: "center" }}
+          >
+            #{index + 1}
+          </Col>
 
-      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            paddingRight: "2%",
-            width: "90%",
-          }}
-        >
-          {sortedSongs.map((song, index) => (
-            <div
-              key={index}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                margin: "10px 0",
-              }}
-            >
-              <div
-                style={{
-                  fontSize: "30px",
-                  fontWeight: 500,
-                  width: "50px",
-                  textAlign: "center",
-                }}
-              >
-                #{index + 1}
-              </div>
-
-              <div
-                style={{
-                  flex: 1,
-                  backgroundColor: "#1F1F1F",
-                  borderRadius: "10px",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                  gap: "60px",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    width: "25%",
-                  }}
-                >
-                  <img
-                    src={import.meta.env.BASE_URL+song.cover}
-                    alt={song.title}
-                    style={{
-                      width: "70px",
-                      height: "70px",
-                      borderRadius: "10px",
-                    }}
-                  />
-                  <div style={{ marginLeft: "10px" }}>
-                    <p
-                      style={{
-                        margin: "5px 0",
-                        fontWeight: 600,
-                        fontSize: "120%",
-                      }}
-                    >
-                      {song.title}
-                    </p>
-                    <p
-                      style={{
-                        margin: 0,
-                        color: "#929292",
-                        fontSize: "12px",
-                      }}
-                    >
-                      {song.artist}
-                    </p>
-                  </div>
-                </div>
-
-                {columns.map((col, i) => (
-                  <div
-                    key={i}
-                    style={{
-                      width: col.width || "150px",
-                      textAlign: "center",
-                      fontWeight: 400,
-                      fontSize: "14px",
-                    }}
-                  >
-                    {typeof col.render === "function"
-                      ? col.render(song, index)
-                      : null}
-                  </div>
-                ))}
+          <Col span={8}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <img
+                src={import.meta.env.BASE_URL + song.cover}
+                alt={song.title}
+                style={{ width: 75, height: 75, borderRadius: 10 }}
+              />
+              <div style={{ marginLeft: 12 }}>
+                <p style={{ margin: 0, fontWeight: 600, fontSize: 20 }}>
+                  {song.title}
+                </p>
+                <p style={{ margin: 0, color: "#929292", fontSize: 13 }}>
+                  {song.artist}
+                </p>
               </div>
             </div>
+          </Col>
+
+          {columns.map((col, i) => (
+            <Col
+              key={i}
+              span={col.span || 4}
+              style={{ textAlign: "center", fontSize: 14 }}
+            >
+              {typeof col.render === "function"
+                ? col.render(song, index)
+                : null}
+            </Col>
           ))}
-        </div>
-      </div>
+        </Row>
+      ))}
     </div>
   );
 };
